c22937090cb1161dda95d0ad34a60a97a660726d
jessev14 <jessevo321@gmail.com>
2024-07-15 10:22:13 -0400

    add empires to containers

--- scripts/empire-economy.js
+++ scripts/empire-economy.js
@@ -32,6 +32,7 @@ Hooks.once('init', () => {
     });
 
     libWrapper.register(moduleID, 'dnd5e.dataModels.actor.AttributesFields.prepareEncumbrance', newPrepareEncumbrance, 'WRAPPER');
+    libWrapper.register(moduleID, 'dnd5e.dataModels.item.ContainerData.prototype.currencyWeight', newGetCurrencyWeight, 'WRAPPER');
 });
 
 
@@ -66,6 +67,38 @@ Hooks.on('renderActorSheet5eCharacter2', (app, [html], appData) => {
     html.querySelector('section.currency').appendChild(empireSelect);
 });
 
+Hooks.on('renderContainerSheet', (app, [html], appData) => {
+    const { item } = app;
+    const currentEmpire = parseInt(item.getFlag(moduleID, 'currentEmpire') ?? 0);
+
+    const empireSelect = document.createElement('select');
+    empireSelect.name = `flags.${moduleID}.currentEmpire`;
+    empireSelect.style.width = '100px';
+    empireSelect.style.margin = 'auto';
+    empireSelect.innerHTML = ``;
+    for (let empireID = 0; empireID < 3; empireID++) {
+        const empireName = game.settings.get(moduleID, `empireName${empireID}`);
+        empireSelect.innerHTML += `<option value="${empireID}" ${empireID === currentEmpire ? 'selected' : ''}>${empireName}</option>`;
+    }
+    empireSelect.addEventListener('change', event => {
+        return item.update({
+            flags: {
+                [moduleID]: {
+                    currentEmpire: event.target.value,
+                    [`empire${currentEmpire}`]: item.system.currency
+                }
+            },
+            system: {
+                currency: item.getFlag(moduleID, `empire${event.target.value}`) ?? {
+                    "pp": 0, "gp": 0, "ep": 0, "sp": 0, "cp": 0
+                }
+            }
+        });
+    });
+    html.querySelector('ol.currency').appendChild(empireSelect);
+});
+
+
 function newPrepareEncumbrance(wrapped, rollData, { validateItem } = {}) {
     wrapped(rollData, validateItem);
 
@@ -98,4 +131,26 @@ function newPrepareEncumbrance(wrapped, rollData, { validateItem } = {}) {
     }
 
     encumbrance.value = weight.toNearest(0.1);
+    encumbrance.pct = Math.clamp((encumbrance.value * 100) / encumbrance.max, 0, 100);
+}
+
+function newGetCurrencyWeight(wrapped) {
+    let weight = wrapped();
+    if (!game.settings.get('dnd5e', 'currencyWeight')) return weight;
+
+    const currentEmpire = parseInt(this.parent?.getFlag(moduleID, 'currentEmpire') ?? 0);
+    for (let empireID = 0; empireID < 3; empireID++) {
+        if (empireID === currentEmpire) continue;
+
+        const currency = this.parent?.getFlag(moduleID, `empire${empireID}`) ?? {
+            "pp": 0, "gp": 0, "ep": 0, "sp": 0, "cp": 0
+        };
+        const count = Object.values(currency).reduce((count, value) => count + value, 0);
+        const currencyPerWeight = game.settings.get("dnd5e", "metricWeightUnits")
+          ? CONFIG.DND5E.encumbrance.currencyPerWeight.metric
+          : CONFIG.DND5E.encumbrance.currencyPerWeight.imperial;
+        weight += count / currencyPerWeight;    
+    }
+
+    return weight;
 }
