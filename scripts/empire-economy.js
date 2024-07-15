b65f3505b485a39b99aaf658d933f1ef5326fd88
jessev14 <jessevo321@gmail.com>
2024-07-14 21:17:56 -0400

    currency weight
    adds libwrapper dependency

--- scripts/empire-economy.js
+++ scripts/empire-economy.js
@@ -30,6 +30,8 @@ Hooks.once('init', () => {
         type: String,
         default: 'Empire 3'
     });
+
+    libWrapper.register(moduleID, 'dnd5e.dataModels.actor.AttributesFields.prepareEncumbrance', newPrepareEncumbrance, 'WRAPPER');
 });
 
 
@@ -63,3 +65,37 @@ Hooks.on('renderActorSheet5eCharacter2', (app, [html], appData) => {
     });
     html.querySelector('section.currency').appendChild(empireSelect);
 });
+
+function newPrepareEncumbrance(wrapped, rollData, { validateItem } = {}) {
+    wrapped(rollData, validateItem);
+
+    if (!game.settings.get('dnd5e', 'currencyWeight')) return;
+
+    const encumbrance = this.attributes.encumbrance;
+    if (!encumbrance) return;
+
+    const config = CONFIG.DND5E.encumbrance;
+    const baseUnits = CONFIG.DND5E.encumbrance.baseUnits[this.parent.type]
+        ?? CONFIG.DND5E.encumbrance.baseUnits.default;
+    const unitSystem = game.settings.get("dnd5e", "metricWeightUnits") ? "metric" : "imperial";
+
+    let weight = encumbrance.value;
+
+    const currentEmpire = parseInt(this.parent?.getFlag(moduleID, 'currentEmpire') ?? 0);
+    for (let empireID = 0; empireID < 3; empireID++) {
+        if (empireID === currentEmpire) continue;
+
+        const currency = this.parent?.getFlag(moduleID, `empire${empireID}`) ?? {
+            "pp": 0, "gp": 0, "ep": 0, "sp": 0, "cp": 0
+        };
+        const numCoins = Object.values(currency).reduce((val, denom) => val + Math.max(denom, 0), 0);
+        const currencyPerWeight = config.currencyPerWeight[unitSystem];
+        weight += dnd5e.utils.convertWeight(
+            numCoins / currencyPerWeight,
+            config.baseUnits.default[unitSystem],
+            baseUnits[unitSystem]
+        );
+    }
+
+    encumbrance.value = weight.toNearest(0.1);
+}
